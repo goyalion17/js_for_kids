abfcf31b4791f3a85de06865349b744112b4ee43
Irina Saveleva <goyalion17@gmail.com>
2023-10-31 22:01:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+// 31.10.2023 ===============================================================
+// малюємо котиків, скільки бажаємо
+/* const drawCats = function (howManyTimes) {
+  for (let i = 0; i < howManyTimes; i++) {
+    console.log(i + "=^.^=");
+  }
+};
+drawCats(10); */
+
+// визначаємо мій вік у секундах
+/* const secondsInAMinute = 60;
+const minutesInAnHour = 60;
+const secondsInAnHour = secondsInAMinute * minutesInAnHour;
+console.log('secondsInAnHour: ', secondsInAnHour);
+const hoursInADay = 24;
+const secondsInADay = hoursInADay * secondsInAnHour;
+console.log('secondsInADay: ', secondsInADay);
+const daysInAYear = 365;
+const secondsInAYear = daysInAYear * secondsInADay;
+console.log('secondsInAYear: ', secondsInAYear);
+let myAge = 36
+console.log(myAge * secondsInAYear) */
+
+// дай п'ять
+/* let highFives = 0
+console.log(++highFives)
+console.log(++highFives)
+console.log(--highFives)
+
+highFives = 0
+console.log(highFives++)
+console.log(highFives++)
+console.log(highFives) */
+
+/* let balloons = 100;
+console.log(balloons *= 2)
+
+balloons = 100;
+console.log(balloons /= 4) */
+
+// Strings
+/* console.log("hgggghgkjslajffaff".length)
+
+const myName = "Iryna"
+console.log(myName[0])
+console.log(myName[1])
+console.log(myName[2])
+console.log(myName[3])
+console.log(myName[4]) */
+
+/* const codeWord1 = "обернись"
+const codeWord2 = "неужели"
+const codeWord3 = "огурцы"
+const codeWord4 = "липкие"
+const codeWord5 = "?!"
+
+console.log(codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]) */
+
+// Slice
+/* console.log("text".slice(1, 3))
+console.log("text".slice(1))
+console.log("text".toUpperCase())
+
+const string = "hey, how are you?"
+const newString = string[0].toUpperCase() + string.slice(1).toLowerCase()
+console.log('newString: ', newString); */
+
+// &&
+/* const hadShower = true;
+const hasBackpack = true;
+console.log(hadShower && hasBackpack);
+
+// ||
+const hasApple = true;
+const hasOrange = false;
+console.log(hasApple || hasOrange)
+
+// - !
+const isWeekend = true;
+const needToShowerToday = !isWeekend;
+console.log(needToShowerToday) */
