c0ac62fb7e1b22c360162df01d9712363e2924b8
Irina Saveleva <goyalion17@gmail.com>
2023-11-01 17:11:09 +0100

    array methods

--- script.js
+++ script.js
@@ -79,3 +79,100 @@ console.log(hasApple || hasOrange)
 const isWeekend = true;
 const needToShowerToday = !isWeekend;
 console.log(needToShowerToday) */
+
+// 1.11.2023 ===============================================================
+// ===
+/* const age = 11;
+const seeMovie = function (age, accompanied) {
+  if (age >= 12 || accompanied === true) {
+    console.log("Ти можешь дивитися цей фільм");
+  } else {
+    console.log("Ти замалий для цього фільму");
+  }
+};
+seeMovie(8, false) */
+
+// Array
+/* const animals = ["Кот", "Пес", "Лама"];
+console.log(animals[0]); */
+
+// додати новий елемент до масиву першим - unshift
+/* animals.unshift("Мартышка");
+console.log(animals);
+animals.unshift("Белый медведь");
+console.log(animals.length);
+console.log(animals[0]);
+console.log(animals.slice(-1));
+console.log(animals.at(-1)); */
+
+// pop
+/* const lastAnimal = animals.pop();
+console.log("lastAnimal: ", lastAnimal);
+console.log(animals);
+console.log(animals.pop());
+animals.unshift(lastAnimal);
+console.log(animals);
+ */
+// shift
+/* const firstAnimal = animals.shift();
+console.log(firstAnimal);
+console.log(animals);
+ */
+// concat
+/* console.log(animals.concat(firstAnimal)); */
+
+// indexOf
+/* const colors = ["красный", "зеленый", "синий"];
+console.log(colors.indexOf("синий"));
+console.log(colors.indexOf("зеленый"));
+console.log(colors.indexOf("фиолетовый")); */
+
+// join - перетворення масиву в строку
+/* const boringAnimals = ["Мартышка", "Кот", "Рыба", "Ящерица"];
+console.log(boringAnimals.join("-"));
+console.log(boringAnimals.join("*"));
+console.log(boringAnimals.join(","));
+
+const myNames = ["Марій", "Марія", "Маріївна"];
+console.log(myNames.join(" ")); */
+
+/* const landmarks = [];
+landmarks.push("Мой дом");
+landmarks.push("Дорожка к дому");
+landmarks.push("Мигающий фонарь");
+landmarks.push("Протекающий гидрант");
+landmarks.push("Пожарная станция");
+landmarks.push("Приют для кошек");
+landmarks.push("Моя бывшая школа");
+landmarks.push("Дом подруги");
+
+console.log(landmarks.pop());
+console.log(landmarks.pop());
+console.log(landmarks.pop());
+console.log(landmarks.pop());
+console.log(landmarks.pop());
+console.log(landmarks.pop());
+console.log(landmarks.pop());
+console.log(landmarks.pop()); */
+
+// Math.random();
+/* console.log(Math.random());
+console.log(Math.random() * 10);
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.floor(Math.random() * 4) + 1);
+console.log(Math.floor(Math.random() * 4) + 1);
+console.log(Math.floor(Math.random() * 4) + 1);
+console.log(Math.floor(Math.random() * 4) + 1);
+console.log(Math.floor(Math.random() * 4) + 1); */
+
+// куля долі
+/* const phrases = [
+  "Звучит неплохо",
+  "Да, это определенно надо сделать",
+  "Не думаю, что это хорошая идея",
+  "Может, не сегодня?",
+  "Компьютер говорит нет",
+];
+// Мне выпить еще молочного коктейля?
+console.log(phrases[Math.floor(Math.random() * phrases.length - 1) + 1]); */
